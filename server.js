225b197a7a9be44f53910d7b8f91a62fe754e2b0
kaileighm <kaileigh@swingleft.org>
2020-10-24 16:06:45 -0700

    create static uploads folder to hold images

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
+app.use(express.static('uploads'));
 
 //start app 
 const port = process.env.PORT || 3000;
